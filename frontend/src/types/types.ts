bd1bea5a2e416048171c916d4b4ff9c467febd16
Juan Manuel Cerdeira <juanmanuelcerdeira860@gmail.com>
2026-02-16 10:49:05 -0300

    dev(frontend): create tag and note types

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,14 @@
+export interface Tag {
+    id: number;
+    name: string;
+}
+
+export interface Note {
+    id: number;
+    title: string;
+    content: string;
+    isArchived: boolean;
+    createdAt: string;
+    updatedAt: string;
+    tags: Tag[];
+}
