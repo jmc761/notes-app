d0fab2bd735ce5e2e635037da9fa0b493f3e874f
Juan Manuel Cerdeira <juanmanuelcerdeira860@gmail.com>
2026-02-15 21:58:45 -0300

    feat(backend): setup orm for sqlite database creation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotesModule } from './notes/notes.module';
+import { Note } from './notes/entities/note.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Note],
+      synchronize: true,
+    }),
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
