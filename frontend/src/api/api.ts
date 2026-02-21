45f77ffafa1af74803ec79642abe96d7ff20770d
Juan Manuel Cerdeira <juanmanuelcerdeira761@gmail.com>
2026-02-21 11:35:02 -0300

    set: include new backend web service to frontend axios

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import { type Note, type NoteInput } from '../types/types';
 
 const BASE_URL = window.location.hostname === 'localhost' 
     ? 'http://localhost:3000' 
-    : 'https://ensolvers-note-challenge.onrender.com';
+    : 'https://notes-app-1k9m.onrender.com/';
 
 const API_URL = `${BASE_URL}/api/notes`;
 
